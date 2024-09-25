74a0ab99ec242585feab55a313561e93eea8aa2f
ahmed ibrahim <Ahmed ibrahim@yahoo.com>
2024-09-25 18:06:14 +0300

    Studying dateStructure (Trie) and solve the daily problem

---
+++ src/trie/TrieNode.java
@@ -0,0 +1,36 @@
+package trie;
+
+public class TrieNode {
+
+    TrieNode[] trieNodes ;
+    boolean isWordEnd;
+    int counter;
+
+    public TrieNode(){
+        trieNodes = new TrieNode[26];
+        isWordEnd = false;
+        counter = 0 ;
+    }
+
+    public boolean containInTrie(char c){
+        return trieNodes[c-'a'] != null;
+     }
+
+     public TrieNode get(char c){
+        return trieNodes[c-'a'];
+     }
+
+     public void put(char c){
+        trieNodes[c - 'a'] = new TrieNode();
+     }
+
+     public void increase(char c){
+        trieNodes[c-'a'].counter++;
+     }
+     public int retrieveCounter(char c){
+        return trieNodes[c-'a'].counter;
+     }
+
+
+
+}
