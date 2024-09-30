aea2bfa08ed2881e424d8cec03f98d34a5eeb90f
ahmed ibrahim <Ahmed ibrahim@yahoo.com>
2024-09-30 15:59:40 +0300

    Stack Problem

---
+++ src/Stack_Queues/CustomStack.java
@@ -0,0 +1,41 @@
+package Stack_Queues;
+
+public class CustomStack {
+    int arr[];
+    int top;
+    int length;
+    public CustomStack(int maxSize){
+     arr = new int [maxSize];
+     length = 0 ;
+     top = -1 ;
+    }
+    public void push(int x){
+        if (isFull()){
+         return;
+        }
+        arr[++top] = x;
+        length++;
+    }
+    public int pop(){
+        if (isEmpty()){
+            return -1;
+        }
+        length--;
+        return arr[top--];
+    }
+    public void increment(int k , int val){
+      for (int i = 0 ; i < Math.min( k , top + 1) ; i++){
+         arr[i] += val;
+      }
+    }
+    public boolean isEmpty(){
+       return length == 0 ;
+    }
+    public boolean isFull(){
+       return length == arr.length ;
+    }
+
+    }
+
+
+
